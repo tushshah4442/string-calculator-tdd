2413d42c4d88ab2b82d3e4ef4e35d307fb3c7d9e
tushar <tushar.shah>
2026-01-05 18:08:14 +0530

    test: support newline as delimiter

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,8 +2,8 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('returns sum of two comma-separated numbers', () {
-    final result = add('1,2,3');
+  test('supports newline as delimiter along with comma', () {
+    final result = add('1\n2,3');
     expect(result, 6);
   });
 }
