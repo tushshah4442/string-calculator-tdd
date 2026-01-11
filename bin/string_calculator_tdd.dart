3f3a99fcd6f7d49f08f89ba449f3fe34b5864ccc
tushar <tushar.shah>
2026-01-11 10:54:51 +0530

    chore: initial dart project setup

---
+++ bin/string_calculator_tdd.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_tdd/string_calculator_tdd.dart' as string_calculator_tdd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_tdd.calculate()}!');
+}
---
+++ test/string_calculator_tdd_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_tdd/string_calculator_tdd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
