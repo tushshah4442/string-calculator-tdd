484aa1e9aa99243cdf83b5e1a7f8d4569fde26fe
tushar <tushar.shah>
2026-01-11 15:33:04 +0530

    test: multiple delimiters should be supported (RED)

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -51,5 +51,10 @@ void main() {
     final calculator = StringCalculator();
     expect(calculator.add("//[***]\n1***2***3"), 6);
   });
+  //supports multiple delimiters
+  test('supports multiple delimiters', () {
+    final calculator = StringCalculator();
+    expect(calculator.add("//[*][%]\n1*2%3"), 6);
+  });
 
 }
