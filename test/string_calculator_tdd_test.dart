50453cc6518579d5e825bb417744bbe14c253067
tushar <tushar.shah>
2026-01-11 12:04:30 +0530

    test: single number returns itself (RED)

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,9 +2,15 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
+  //for empty string
   test('returns 0 for empty string', () {
     final calculator = StringCalculator();
     final result = calculator.add('');
     expect(result, 0);
+  });
+  //pass single number
+  test('single number returns the number itself', () {
+    final calculator = StringCalculator();
+    expect(calculator.add('1'), 1);
   });
 }
