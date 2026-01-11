d10896b4ab2677776027ae3f9c895f2e487e2a73
tushar <tushar.shah>
2026-01-11 12:17:04 +0530

    feat: support two comma separated numbers (GREEN)

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,9 +1,13 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) {
-      return 0;
+    if (numbers.isEmpty) return 0;
+
+    if (numbers.contains(',')) {
+      final parts = numbers.split(',');
+      return int.parse(parts[0]) + int.parse(parts[1]);
     }
 
     return int.parse(numbers);
   }
+
 }
